f4b5e2c699323a39e0f914f78d007c794270c4e0
HashiniPrabuddhika <prabuddhikarph@gmail.com>
2026-08-08 14:12:53 +0530

    feat: add cart and auth state context

---
+++ userpanelfront/src/context/StoreContext.jsx
@@ -0,0 +1,78 @@
+import { createContext, useState, useEffect } from "react";
+import { fetchFoodList } from "../service/Productservice";
+import axios from "axios";
+import {
+  addToCart,
+  getCartData,
+  removeQtyFromCart,
+} from "../service/cartService";
+
+export const StoreContext = createContext(null);
+
+export const StoreContextProvider = (props) => {
+  const [foodList, setFoodList] = useState([]);
+  const [quantities, setQuantities] = useState({});
+  const [token, setToken] = useState("");
+
+  const increaseQuantity = async (foodId) => {
+    setQuantities((prev) => ({ ...prev, [foodId]: (prev[foodId] || 0) + 1 }));
+
+    await addToCart(foodId, token);
+  };
+
+  const decreaseQuantity = async (foodId) => {
+    setQuantities((prev) => ({
+      ...prev,
+      [foodId]: prev[foodId] > 0 ? prev[foodId] - 1 : 0,
+    }));
+
+    await removeQtyFromCart(foodId, token);
+  };
+
+  const removeFromCart = (foodId) => {
+    setQuantities((prevQuantities) => {
+      const updatedQuantities = { ...prevQuantities };
+      delete updatedQuantities[foodId];
+      return updatedQuantities;
+    });
+  };
+
+  const loadCartData = async (token) => {
+    const items = await getCartData(token);
+    setQuantities(items);
+  };
+
+  const contextValue = {
+    foodList,
+    quantities,
+    increaseQuantity,
+    decreaseQuantity,
+    removeFromCart,
+    token,
+    setToken,
+    loadCartData,
+    setQuantities,
+  };
+
+  useEffect(() => {
+    async function loadData() {
+      const data = await fetchFoodList();
+      setFoodList(data);
+      if (localStorage.getItem("token")) {
+        const storedToken = localStorage.getItem("token");
+        if (storedToken) {
+          setToken(storedToken);
+          await loadCartData(storedToken);
+        }
+      }
+    }
+
+    loadData();
+  }, []);
+
+  return (
+    <StoreContext.Provider value={contextValue}>
+      {props.children}
+    </StoreContext.Provider>
+  );
+};
