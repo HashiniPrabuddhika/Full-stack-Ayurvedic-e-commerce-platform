91d54f7759bcadcad8952d8cbc873a8f2dc35919
HashiniPrabuddhika <prabuddhikarph@gmail.com>
2026-08-08 14:12:48 +0530

    feat: add App component with routing

---
+++ userpanelfront/src/App.jsx
@@ -0,0 +1,44 @@
+import React, { useContext } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+
+import Menubar from './components/Menubar/Menubar';
+import Footer from './components/Footer/Footer';
+import Home from './pages/Home/Home';
+import Contact from './pages/Contact/Contact';
+import ExploreFood from './pages/ExploreProduct/ExploreProduct';
+import FoodDetail from './pages/ProductDetails/ProductDetail';
+import Cart from './pages/Cart/Cart';
+import PlaceOrder from './pages/PlaceOrder/PlaceOrder';
+import Login from './components/Login/Login';
+import Register from './components/Register/Register';
+import OrderStatus from './pages/OrderStatus/OrderStatus';
+import MyOrders from './pages/MyOrders/MyOrders';
+
+import { StoreContext } from './context/StoreContext';
+
+const App = () => {
+  const { token } = useContext(StoreContext);
+
+  return (
+    <div>
+      <Menubar />
+      <ToastContainer />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/explore" element={<ExploreFood />} />
+        <Route path="/food/:id" element={<FoodDetail />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/order" element={token ? <PlaceOrder /> : <Login />} />
+        <Route path="/login"  element={token ? <Home /> : <Login />} />
+        <Route path="/register" element={token ? <Home /> : <Register />} />
+        <Route path="/status" element={<OrderStatus />} />
+        <Route path="/myorders" element={token ? <MyOrders /> : <Login />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
